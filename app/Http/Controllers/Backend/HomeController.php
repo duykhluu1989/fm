77fb83ce5b2d3fa295b80af14e6b181816b6f713
duykhluu <duykhluu1989@gmail.com>
2017-09-04 21:20:02 +0700

    Dashboard

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\OrderAddress;
 
 class HomeController extends Controller
 {
@@ -14,8 +15,26 @@ class HomeController extends Controller
             ->get()
             ->toArray();
 
+        $orderByDistricts = DB::table('order')
+            ->select(DB::raw('order_address.district, count(order.id) as total, count(if(order.completed_at is not null, order.id, null)) as complete, count(if(order.failed_at is not null, order.id, null)) as fail, count(if(order.cancelled_at is not null, order.id, null)) as cancel, sum(if(order.completed_at is not null, order.weight, 0)) as weight, sum(if(order.completed_at is not null, order.cod_price, 0)) as cod_price, sum(if(order.completed_at is not null, order.shipping_price, 0)) as shipping_price'))
+            ->join('order_address', function($join) {
+                $join->on('order.id', '=', 'order_address.order_id')->where('order_address.type', '=', OrderAddress::TYPE_SENDER_DB);
+            })->groupBy('order_address.district_id')
+            ->orderBy('order_address.district_id')
+            ->get()
+            ->toArray();
+
+        $orderByShippers = DB::table('order')
+            ->select(DB::raw('shipper, count(order.id) as total, count(if(order.completed_at is not null, order.id, null)) as complete, count(if(order.failed_at is not null, order.id, null)) as fail, count(if(order.cancelled_at is not null, order.id, null)) as cancel, sum(if(order.completed_at is not null, order.weight, 0)) as weight, sum(if(order.completed_at is not null, order.cod_price, 0)) as cod_price, sum(if(order.completed_at is not null, order.shipping_price, 0)) as shipping_price'))
+            ->groupBy('shipper')
+            ->orderBy('shipper')
+            ->get()
+            ->toArray();
+
         return view('backend.homes.home', [
             'orders' => $orders[0],
+            'orderByDistricts' => $orderByDistricts,
+            'orderByShippers' => $orderByShippers,
         ]);
     }
 
--- resources/views/backend/homes/home.blade.php
+++ resources/views/backend/homes/home.blade.php
@@ -41,31 +41,83 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="fa fa-dropbox"></i></span>
-                <div class="info-box-content">
-                    <span class="info-box-text">Tổng Khối Lượng</span>
-                    <span class="info-box-number">{{ \App\Libraries\Helpers\Utility::formatNumber($orders->weight) }}</span>
+    </div>
+
+    <div class="box box-primary">
+        <div class="box-body">
+            <div class="row">
+                <div class="col-sm-4 col-xs-6">
+                    <div class="description-block">
+                        <span class="description-text text-green">Tổng Khối Lượng</span>
+                        <h5 class="description-header">{{ \App\Libraries\Helpers\Utility::formatNumber($orders->weight) }}</h5>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+                    <div class="description-block">
+                        <span class="description-text text-green">Tổng Thu Hộ</span>
+                        <h5 class="description-header">{{ \App\Libraries\Helpers\Utility::formatNumber($orders->cod_price) }}</h5>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+                    <div class="description-block">
+                        <span class="description-text text-green">Tổng Phí Ship</span>
+                        <h5 class="description-header">{{ \App\Libraries\Helpers\Utility::formatNumber($orders->shipping_price) }}</h5>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="fa fa-envelope"></i></span>
-                <div class="info-box-content">
-                    <span class="info-box-text">Tổng Thu Hộ</span>
-                    <span class="info-box-number">{{ \App\Libraries\Helpers\Utility::formatNumber($orders->cod_price) }}</span>
-                </div>
+    </div>
+
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title">Location</h3>
+        </div>
+        <div class="box-body">
+            <div class="row">
+
+                @foreach($orderByDistricts as $orderByDistrict)
+                    <div class="col-sm-3 col-xs-6">
+                        <div class="description-block">
+                            <span class="description-text text-green">{{ $orderByDistrict->district }}</span>
+                        </div>
+                        <h4>Tổng Đơn Hàng: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByDistrict->total) }}</b></h4>
+                        <h4>Hoàn Thành: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByDistrict->complete) }}</b></h4>
+                        <h4>Không Giao Được: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByDistrict->fail) }}</b></h4>
+                        <h4>Hủy: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByDistrict->cancel) }}</b></h4>
+                        <h4>Tổng Khối Lượng: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByDistrict->weight) }}</b></h4>
+                        <h4>Tổng Thu Hộ: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByDistrict->cod_price) }}</b></h4>
+                        <h4>Tổng Phí Ship: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByDistrict->shipping_price) }}</b></h4>
+                    </div>
+                @endforeach
+
             </div>
         </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="fa fa-money"></i></span>
-                <div class="info-box-content">
-                    <span class="info-box-text">Tổng Phí Ship</span>
-                    <span class="info-box-number">{{ \App\Libraries\Helpers\Utility::formatNumber($orders->shipping_price) }}</span>
-                </div>
+    </div>
+
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title">Shipper</h3>
+        </div>
+        <div class="box-body">
+            <div class="row">
+
+                @foreach($orderByShippers as $orderByShipper)
+                    @if(!empty($orderByShipper->shipper))
+                        <div class="col-sm-3 col-xs-6">
+                            <div class="description-block">
+                                <span class="description-text text-green">{{ $orderByShipper->shipper }}</span>
+                            </div>
+                            <h4>Tổng Đơn Hàng: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByShipper->total) }}</b></h4>
+                            <h4>Hoàn Thành: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByShipper->complete) }}</b></h4>
+                            <h4>Không Giao Được: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByShipper->fail) }}</b></h4>
+                            <h4>Hủy: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByShipper->cancel) }}</b></h4>
+                            <h4>Tổng Khối Lượng: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByShipper->weight) }}</b></h4>
+                            <h4>Tổng Thu Hộ: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByShipper->cod_price) }}</b></h4>
+                            <h4>Tổng Phí Ship: <b>{{ \App\Libraries\Helpers\Utility::formatNumber($orderByShipper->shipping_price) }}</b></h4>
+                        </div>
+                    @endif
+                @endforeach
+
             </div>
         </div>
     </div>
