622a19ebfcb87f04d8f8ec3e08ef2be1951f9bc3
Duy Luu <duykhluu1989@gmail.com>
2017-09-07 14:11:44 +0700

    Menu

--- resources/views/frontend/layouts/partials/menu.blade.php
+++ resources/views/frontend/layouts/partials/menu.blade.php
@@ -9,7 +9,7 @@
                             <li>
                                 <a href="{{ action('Frontend\UserController@editAccount') }}" data-toggle="dropdown" class="dropdown-toggle"><i class="fa fa-user" aria-hidden="true"></i>  Xin chào {{ auth()->user()->name }} <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
-                                    <li class="{{ (request()->is('account') ? 'active' : '') }}"><a href="{{ action('Frontend\UserController@editAccount') }}"><i class="fa fa-user" aria-hidden="true"></i> Thông tin tài khoản</a></li>
+                                    <li class="{{ (request()->is('account') ? 'active' : '') }}"><a href="{{ action('Frontend\UserController@editAccount') }}"><i class="fa fa-info-circle" aria-hidden="true"></i> Thông tin tài khoản</a></li>
                                     <li class="{{ (request()->is('account/general') ? 'active' : '') }}"><a href="{{ action('Frontend\UserController@general') }}"><i class="fa fa-tasks" aria-hidden="true"></i> Tổng quan chung</a></li>
                                     <li class="{{ (request()->is('account/order') ? 'active' : '') }}"><a href="{{ action('Frontend\UserController@adminOrder') }}"><i class="fa fa-th-list" aria-hidden="true"></i> Quản lý đơn hàng</a></li>
                                     <li class="{{ (request()->is('order/import') ? 'active' : '') }}"><a href="{{ action('Frontend\OrderController@importExcelPlaceOrder') }}"><i class="fa fa-file-excel-o" aria-hidden="true"></i> Đăng đơn hàng bằng excel</a></li>
